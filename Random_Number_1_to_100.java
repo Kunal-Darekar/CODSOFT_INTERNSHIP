3b72b5322be31bf9e2433d82fbde149d7e5f87b9
Kunal Darekar <150500530+Kunal-Darekar@users.noreply.github.com>
2023-12-09 23:08:02 +0530

    Add files via upload

---
+++ Random_Number_1_to_100.java
@@ -0,0 +1,11 @@
+
+//Generate a random number within a specified range, such as 1 to 100.
+import java.util.Random;
+
+public class Random_Number_1_to_100 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(100) + 1;
+        System.out.println("Random number between 1 and 100 is:" + randomNumber);
+    }
+}
